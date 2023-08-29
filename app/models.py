37d7072df782dcae8522e1ccc412d74d01cf6265
dvir <dvirsb@gmail.com>
2023-08-29 12:13:46 +0300

    NEED FIX

---
+++ app/models.py
@@ -0,0 +1,12 @@
+# app/models.py
+from datetime import datetime
+from app import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    due_date = db.Column(db.DateTime, nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    
+    def __repr__(self):
+        return f"Task('{self.title}', '{self.due_date}', '{self.completed}')"
